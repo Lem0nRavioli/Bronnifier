8ef8aef650707566e6d503a8fbf2fdc908ae2f21
Lem0nRavioli <hirona_mj@hotmail.fr>
2020-11-30 17:09:25 +0100

    added gui

--- bronnify.py
+++ bronnify.py
@@ -4,13 +4,14 @@ face_classifier = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_fro
 
 bronn = cv2.imread("bronn_clean.png", cv2.IMREAD_UNCHANGED)
 # bronn = bronn[134:670, 85:530]  # trunked top head + chin + thin face
-bronn = bronn[90:690, 85:530]  # little cut top of head + chin + thin face
+# bronn = bronn[90:690, 85:530]  # little cut top of head + chin + thin face
+# bronn = bronn[90: 650, 85:520]
 
 
-def infect_bronn(pic, face_loc):
+def infect_bronn(pic, face_loc, x_offset, y_offset):
     y, x, h, w = face_loc
     face_square = pic[x:x+w, y:y+h].copy()
-    bronn_copy = bronn.copy()
+    bronn_copy = bronn[90: 690 + x_offset, 85:520 + y_offset].copy()
     bronn_copy = cv2.resize(bronn_copy, (w, h))
 
     for i in range(w):
@@ -22,22 +23,30 @@ def infect_bronn(pic, face_loc):
     return pic
 
 
-def spread_bronn(img_path, save_name, sensitivity=3):
+def detect_victims(img_path, sensitivity=3):
     img = cv2.imread(img_path)
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_classifier.detectMultiScale(img_gray, 1.25, sensitivity)
-    for face in faces:
-        img = infect_bronn(img, face)
+    return faces
 
-    cv2.imwrite('bronn_paradise/' + save_name + '.jpg', img)
+
+def spread_bronn(img_path, faces, x_offset, y_offset):
+    img = cv2.imread(img_path)
+    for face in faces:
+        img = infect_bronn(img, face, x_offset, y_offset)
     return img
 
 
-"""
-Save the pictures in the "faces" folder 
-Change the path / save name and run
-"""
+def id_save_her(img_path, save_name, faces, x_offset, y_offset):
+    img = spread_bronn(img_path, faces, x_offset, y_offset)
+    cv2.imwrite(save_name, img)
 
-img_path = "faces/<image_name>.jpg"
-img_save_name = "<picture_saved_name>"
-spread_bronn(img_path, img_save_name)
+
+# """
+# Save the pictures in the "faces" folder
+# Change the path / save name and run
+# """
+#
+# img_path = "faces/<image_name>.jpg"
+# img_save_name = "<picture_saved_name>"
+# spread_bronn(img_path, img_save_name)
